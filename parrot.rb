2a8ed6e3c3bcf531bae6fbd5c073c7c764f92350
Kelvin <kmwendwa476@gmail.com>
2022-08-19 09:18:36 +0300

    commit tree 6577a873c9de516ab7c1906da9d4da40356fd966

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(default_value = "Squawk!")
+  puts default_value
+  return default_value
+end
+
+parrot("mbona")
